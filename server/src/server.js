c5945718bd3344c6d05b920d56828986cb270bcc
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2021-03-31 20:53:42 +0300

    Add /users/:userId route

--- server/src/server.js
+++ server/src/server.js
@@ -8,9 +8,15 @@ const PORT = 3000;
 const HOST = '0.0.0.0';
 
 const app = express();
+
 app.get('/', (req, res) => {
-  res.send('Hello World');
+    res.send('Hello World');
 });
+
+app.get('/users/:userId', (req, res) => {
+    res.send(req.params);
+});
+
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
